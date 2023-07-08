eff5269c86e704147e3bcbaa4dcd6beda6a15be3
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 07:24:35 +0300

    utils.mk: restyle

--- utils.mk
+++ utils.mk
@@ -1,12 +1,12 @@
-GREP_DEFS = --exclude-dir=.git --exclude-dir=.github -R .
-FIND_DEFS = -not \( -path "./.git*" -or -path ".*~" \)
-MAXLINE   = 80
+GREPOPT = --exclude-dir=.git --exclude-dir=.github -R .
+FINDOPT = -not \( -path "./.git*" -or -path ".*~" \)
+MAXLINE = 80
 
 urlcodes:
 	@echo "=======> Check URLs for response code"
-	@grep -Eiho "https?://[^\"\\'> ]+" ${GREP_DEFS}    \
-		| xargs -P10 -I{} curl -o /dev/null        \
-		 -sw "[%{http_code}] %{url}\n" '{}'        \
+	@grep -Eiho "https?://[^\"\\'> ]+" ${GREPOPT} \
+		| xargs -P10 -I{} curl -o /dev/null   \
+		 -sw "[%{http_code}] %{url}\n" '{}'   \
 		| sort -u
 
 podchecker:
@@ -15,11 +15,11 @@ podchecker:
 
 shellcheck:
 	@echo "=======> Check shell scripts for syntax errors"
-	@grep -m1 -l '^#\s*!/bin/sh' ${GREP_DEFS} | xargs -L10 shellcheck -s sh
+	@grep -m1 -l '^#\s*!/bin/sh' ${GREPOPT} | xargs -L10 shellcheck -s sh
 
 longlines:
 	@echo "=======> Check for long lines (> ${MAXLINE})"
-	@find . -type f ${FIND_DEFS} -exec awk -v ML=${MAXLINE} \
+	@find . -type f ${FINDOPT} -exec awk -v ML=${MAXLINE} \
 		'length > ML { print FILENAME ":" FNR " " $$0 }'  {} \;
 
 .PHONY: urlcodes podchecker shellcheck longlines
