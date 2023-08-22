b16c70e72477356082736522a60828c26125fabd
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-22 17:27:43 +0300

    config.mk: remove redundant vars

--- config.mk
+++ config.mk
@@ -1,9 +1,6 @@
 # project metadata
 NAME        = mkinitramfs
 VERSION     = 0.3.1
-DESCRIPTION = Initramfs Generator
-HOMEPAGE    = https://github.com/zeppe-lin/mkinitramfs/
-BUGTRACKER  = https://github.com/zeppe-lin/mkinitramfs/issues/
 DIST        = ${NAME}-${VERSION}
 
 # paths
